7e1680ec326efcc717b6484d24a3d18273d183de
macbook <sebastian@kometsales.com>
2018-01-19 18:39:53 -0500

    version to continue

--- components/UserInterface.vue
+++ components/UserInterface.vue
@@ -1,8 +1,8 @@
 <template>  
     <div>
     
-          <v-avatar v-if="loged" @click="goToInspire" size="36px" slot="activator">
-            <img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"  alt="">
+          <v-avatar style="cursor: pointer" v-if="loged" @click="goToInspire" size="36px" slot="activator">
+            <img :src="avatarUrl"  alt="">
           </v-avatar>
           <strong v-if="loged">{{ username }}</strong>                              
           <v-btn v-if="loged" @click="home">
@@ -14,7 +14,7 @@
 
           
           <dialog-login  v-if="!loged"></dialog-login>
-          <v-btn  @click="facebookLogin" v-if="!loged">Facebook</v-btn>
+          <v-btn  @click="facebookLogin" v-if="false">Facebook</v-btn>
           
     </div>
 </template>
@@ -27,6 +27,7 @@ export default {
   data () {
     return {
       username: '',
+      avatarUrl: '',
       loged: false
     }
   },
@@ -48,7 +49,8 @@ export default {
       }).then(function (response) {
         if (response) {
           if (this.getCookie('access_token')) {
-            this.username = response.data
+            this.username = response.data.username
+            this.avatarUrl = response.data.avatar
             this.loged = true
             this.$emit('hasUser')
           }
@@ -82,6 +84,23 @@ export default {
     },
     facebookLogin () {
       window.location.href = 'http://localhost:8080/login/facebook'
+    },
+    logout () {
+      axios({
+        method: 'get',
+        url: 'http://localhost:8080/logouts',
+        headers: {'Content-Type': 'application/json'},
+        params: {
+          access_token: this.getCookie('access_token')
+        }
+      }).then(function (response) {
+        if (response) {
+          this.loged = false
+          this.$emit('logout')
+          this.delete_cookie('access_token')
+          this.$router.go('/')
+        }
+      }.bind(this))
     }
   }
 }
--- pages/indexCards.vue
+++ pages/indexCards.vue
@@ -2,12 +2,32 @@
 
 <div class="flex xs12">
     <v-flex xs12 v-for="post in posts" :key="post.id">
-        <v-card color="blue-grey darken-2" class="white--text">
+        <v-card color="blue-grey darken-2" class="white--text">    
             <v-container fluid grid-list-lg>              
+              <v-layout row wrap> 
+                <v-flex xs2>
+                  <v-avatar  size="36px" slot="activator">
+                    <img :src="post.avatarUrl"  alt="">
+                  </v-avatar>                        
+                </v-flex>
+                <v-flex xs3 d-flex>
+                  <v-layout row wrap class="text-lg-left">                  
+                    <v-flex d-flex class="pb-0 pt-0">                      
+                        <strong>{{post.username}} {{post.lastName}}</strong>                                         
+                    </v-flex>
+                    <v-flex d-flex class="pb-0 pt-0">                       
+                        <strong>{{post.profession}}</strong>                              
+                    </v-flex>
+                    <v-flex d-flex class="pb-0 pt-0">                       
+                        <strong>{{post.createdDate}}</strong>                       
+                    </v-flex>
+                  </v-layout>                       
+                </v-flex>
+              </v-layout>                       
               <v-layout row>          
-                    <v-flex>
-                        <div>{{post.body}}</div>
-                            <v-card-media src="/bby-driver.jpg" height="300px" contain></v-card-media>
+                    <v-flex>            
+                      <v-card-title primary class="title">{{post.body}}</v-card-title>                                  
+                      <v-card-media src="/bby-driver.jpg" height="300px" contain></v-card-media>
                     </v-flex>
               </v-layout>  
             </v-container>
@@ -31,12 +51,11 @@ export default {
     canI: {type: Boolean, required: true}
   },
   mounted () {
-    this.fetchPosts()
+    this.fetchUserData()
   },
   methods: {
-    fetchPosts () {
-      console.log('entro')
-      return axios.get('http://localhost:8080/posts')
+    fetchUserData () {
+      return axios.get('http://localhost:8080/userPost')
         .then(function (response) {
           this.posts = response.data
         }.bind(this))
@@ -45,7 +64,7 @@ export default {
   watch: {
     canI: function () {
       if (this.canI) {
-        this.fetchPosts()
+        this.fetchUserData()
         this.$emit('increment')
       }
     }
